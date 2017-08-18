164701ae72aed718831612d81445c25a28d66d80
Evan Snow <snow@evans-mbp.attlocal.net>
2017-08-18 13:55:15 -0500

    Checkpoints 2-5: Complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,25 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl(Room, Message, $uibModal) {
+        var home = this;
+        home.rooms = Room.all;
+        home.currentRoom = null;
+        
+        home.addRoom = function() {
+            $uibModal.open({
+                templateUrl: '/templates/modal.html',
+                size: 'sm',
+                controller: 'ModalCtrl as modal'
+            });
+        }
+        
+        home.setCurrentRoom = function (room) {
+            home.currentRoom = room;
+            home.messages = Message.getByRoomId(home.currentRoom.$id);
+        }
+        
     }
-
+    
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
